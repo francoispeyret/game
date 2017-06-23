9ce31377e2d981d02704d17d6f7f71ae9e2dd300
fpeyret <fpeyret@axome.com>
2017-06-23 08:12:03 +0200

    [+] Compass / .gitignore

---
+++ config.rb
@@ -0,0 +1,19 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "../"
+css_dir = "css"
+sass_dir = "sass"
+font_dir = "fonts"
+fonts_dir = "fonts"
+images_dir = "img"
+javascripts_dir = "js"
+
+output_style = :expanded
+environment = :production
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
